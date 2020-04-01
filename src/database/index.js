63dd2db69fa4855488c638a6cc9f88d90389bc7c
Virginia Rodrigues Cruz <virginia.cruz@ifactory.com.br>
2020-03-31 22:11:53 -0300

    create appointments and notifications

--- src/database/index.js
+++ src/database/index.js
@@ -1,19 +1,18 @@
 import Sequelize from 'sequelize';
-// import mongoose from 'mongoose';
+import mongoose from 'mongoose';
 
 import User from '../app/models/User';
-// import File from '../app/models/File';
-// import Appointment from '../app/models/Appointment';
+import File from '../app/models/File';
+import Appointment from '../app/models/Appointment';
 
 import databaseConfig from '../config/database';
 
-// const models = [User, File, Appointment];
-const models = [User];
+const models = [User, File, Appointment];
 
 class Database {
   constructor() {
     this.init();
-    // this.mongo();
+    this.mongo();
   }
 
   init() {
@@ -21,19 +20,19 @@ class Database {
 
     models
       .map(model => model.init(this.connection))
-      // .map(model => model.associate && model.associate(this.connection.models));
+      .map(model => model.associate && model.associate(this.connection.models));
   }
 
-  // mongo() {
-  //   this.mongoConnection = mongoose.connect(
-  //     'mongodb://localhost:27017/gobarber',
-  //     {
-  //       useNewUrlParser: true,
-  //       useFindAndModify: true,
-  //       useUnifiedTopology: true,
-  //     }
-  //   );
-  // }
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://localhost:27017/fastfeet',
+      {
+        useNewUrlParser: true,
+        useFindAndModify: true,
+        useUnifiedTopology: true,
+      }
+    );
+  }
 }
 
 export default new Database();
